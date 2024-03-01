898ec621aed05767c5c38edfd26c59f0ea364b91
cbakewell <cbakewell@CartersVM.myguest.virtualbox.org>
2024-03-01 15:31:46 -0600

    client

---
+++ CartsClient.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+	struct addrinfo hints, *res, con;
+	int addr, sockfd, accept;
+
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ((addr =getaddrinfo(argv[1], "8080", &hints, &res)) == -1){
+		fprintf(stderr, "Error: %s", gai_strerror(addr));
+		exit(1);
+	}
+
+	if ((sockfd = socket(res->ai_family, res->ai_socktype, 0)) == -1){
+		printf("Error: %s", strerror(errno));
+		exit(1);
+	}
+
+	if ((accept = connect(sockfd, res->ai_addr, res->ai_addrlen)) == -1){
+		printf("Error: %s", strerror(errno));
+		exit(1);
+	}
+	return 0;
+}
